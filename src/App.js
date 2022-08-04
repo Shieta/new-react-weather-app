81a694c6af50cd96262378b55b2123ce61c4770c
Shieta <98153977+Shieta@users.noreply.github.com>
2022-08-04 21:36:07 +1000

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This is project is created by Shienny and
-        <a
-          href="https://github.com/Shieta/new-react-weather-app"
-          target="_blank"
-        >
-          is open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This is project is created by Shienny and
+          <a
+            href="https://github.com/Shieta/new-react-weather-app"
+            target="_blank"
+          >
+            is open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
